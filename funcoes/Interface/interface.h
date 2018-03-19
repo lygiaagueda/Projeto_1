53e3af4c95234ca317f7e084b2c8bd4c640ddad8
lygiaagueda <37081068+lygiaagueda@users.noreply.github.com>
2018-03-19 14:18:39 -0300

    Atualização interface.h

--- funcoes/Interface/interface.h
+++ funcoes/Interface/interface.h
@@ -1,15 +1,22 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
 void ExibeMenuItem(){
     printf("1 - Cadastrar imovel\n");
     printf("2 - Consutar imoveis disponiveis\n");
     printf("3 - Consultar descricao de todos os imoveis\n");
     printf("4 - Consultar imoveis disponiveis para venda\n");
-    printf("5 - Consutar imoveis disponíveis para alugar\n");
+    printf("5 - Consutar imoveis disponÃ­veis para alugar\n");
     printf("6 - Sair\n\n");
 
 }
 
 void ExibeMenuSubItem1(){
-    printf("1 - Cadastrar casa\n");
+    printf("\n1 - Cadastrar casa\n");
     printf("2 - Cadastrar apartamento\n");
-    printf("3 - Cadastrar terreno\n\n");
+    printf("3 - Cadastrar terreno\n");
+    printf("4 - Voltar\n\n");
 }
+
+#endif
