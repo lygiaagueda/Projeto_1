28dde7701d6c11eb40abc837e7eee73d09f80e0e
lygiaagueda <37081068+lygiaagueda@users.noreply.github.com>
2018-03-19 14:16:53 -0300

    Atualização main

--- main.c
+++ main.c
@@ -1,18 +1,51 @@
 #include "includes.h"
 
+void ImprimeCasa(tCasa *casas, int i){
+    printf("Casa %d:\n", i);
+    printf("\tEndereco: %s\n", casas[i].dados.endereco);
+    printf("\tNumero: %d\n", casas[i].dados.numero);
+    printf("\tBairro %s\n", casas[i].dados.bairro);
+    printf("\tCep: %s\n", casas[i].dados.cep);
+    printf("\tCidade: %s\n", casas[i].dados.cidade);
+    printf("\tValor: %lf\n", casas[i].dados.valor);
+    if(casas[i].dados.disponivelAluVen == 'A'){
+        printf("\tCasa disponivel para aluguel\n");
+    }else{
+        printf("Casa disponivel para venda\n");
+    }
+}
+
+void ImprimeDescricaoCasa(tCasa *casas, int i){
+    printf("\n\tDetalhes da casa %d\n", i);
+    printf("\t%d\n", casas[i].numPavimentos);
+    printf("\t%d\n", casas[i].numQuartos);
+    printf("\t%lf\n", casas[i].areaTerreno);
+    printf("\t%lf\n", casas[i].areaConstruida);
+}
+
+
+void ImoveisDisponiveis(tCasa *casas, tApartamento *apartamentos, tTerreno *terrenos){
+    int i;
+
+    for(i=0; i<100; i++){
+        if(casas[i].dados.disponivelAluVen != 'N' ||casas[i].dados.disponivelAluVen != 'n'){
+            printf("Casa %d disponivel.\n", i);
+        }
+    }
+
+}
+
+
 int main(void){
     tCasa casas[100];
     tApartamento apartamentos[100];
     tTerreno terrenos[100];
-    tImovel imovel[100];
     int opcaoItem, opcaoSubItem;
 
-    imovel[0].numero = 10;
-    printf("%d \n\n", imovel[0].numero);
 
     while(1){
         ExibeMenuItem();
-        printf("Escolha uma opção:" );
+        printf("Escolha uma opção: ");
         scanf("%d", &opcaoItem);
 
         switch(opcaoItem){
@@ -21,6 +54,10 @@ int main(void){
                     ExibeMenuSubItem1();
                     printf("Escolha uma opcao: ");
                     scanf("%d", &opcaoSubItem);
+
+                    if(opcaoSubItem == 4){
+                        break;
+                    }
                         switch(opcaoSubItem){
                             case 1:
                                 CadastraCasa(casas);
@@ -30,12 +67,20 @@ int main(void){
                                 break;
                             case 3:
                                 CadastraTerreno(terrenos);
+                                break;
+                            default:
+                                printf("Opcao invalida.\n");
                         }
+
+
                 }
             //case 2:
         }
+
     }
-    CadastraCasa(casas);
+
+
+
 
     return 0;
 }
