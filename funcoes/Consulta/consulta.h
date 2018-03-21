12d40557f75c36b53c95e64d660c00ac8f155c67
Lenildo Luan <luanmegax.ll@gmail.com>
2018-03-21 15:33:24 -0300

    HaHa

--- funcoes/Consulta/consulta.h
+++ funcoes/Consulta/consulta.h
@@ -1,27 +1,34 @@
 #ifndef CONSULTA_H
 #define CONSULTA_H
 
-void ImprimeCasa(tCasa *casas, int i){
-    printf("Casa %d:\n", i);
-    printf("\tEndereco: %s\n", casas[i].dados.endereco);
-    printf("\tNumero: %d\n", casas[i].dados.numero);
-    printf("\tBairro %s\n", casas[i].dados.bairro);
-    printf("\tCep: %s\n", casas[i].dados.cep);
-    printf("\tCidade: %s\n", casas[i].dados.cidade);
-    printf("\tValor: %lf\n", casas[i].dados.valor);
-    if(casas[i].dados.disponivelAluVen == 'A'){
-        printf("\tCasa disponivel para aluguel\n");
-    }else{
-        printf("Casa disponivel para venda\n");
-    }
-}
+void ImprimeCasa(tCasa casas, FILE *imoveis){
 
+    while(1){
+        fread(&casas, 1, sizeof(tCasa), imoveis);
+
+        if(feof(imoveis)) break;
+
+        printf("Casa:\n");
+        printf("\tEndereco: %s\n", casas.dados.endereco);
+        printf("\tNumero: %d\n", casas.dados.numero);
+        printf("\tBairro %s\n", casas.dados.bairro);
+        printf("\tCep: %s\n", casas.dados.cep);
+        printf("\tCidade: %s\n", casas.dados.cidade);
+        printf("\tValor: %lf\n", casas.dados.valor);
+        if(casas.dados.disponivelAluVen == 'A'){
+            printf("\tCasa disponivel para aluguel\n");
+        }else{
+            printf("Casa disponivel para venda\n");
+        }
+    }    
+}
+/*
 void ImprimeDescricaoCasa(tCasa *casas, int i){
     printf("\n\tDetalhes da casa %d\n", i);
-    printf("\t%d\n", casas[i].numPavimentos);
-    printf("\t%d\n", casas[i].numQuartos);
-    printf("\t%lf\n", casas[i].areaTerreno);
-    printf("\t%lf\n", casas[i].areaConstruida);
+    printf("\t%d\n", casas.numPavimentos);
+    printf("\t%d\n", casas.numQuartos);
+    printf("\t%lf\n", casas.areaTerreno);
+    printf("\t%lf\n", casas.areaConstruida);
 }
 
 
@@ -34,6 +41,6 @@ void ImoveisDisponiveis(tCasa *casas, tApartamento *apartamentos, tTerreno *terr
         }
     }
 
-}
+}*/
 
 #endif
