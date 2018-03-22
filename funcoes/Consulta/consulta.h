c1c837f917322cb607426f73d1edfdbe61107870
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2018-03-22 14:28:42 -0300

    Update consulta.h
    topicos 2, 3, e 4 complestos (na teoria)

--- funcoes/Consulta/consulta.h
+++ funcoes/Consulta/consulta.h
@@ -23,23 +23,141 @@ void ImprimeCasa(tCasa casas, FILE *imoveis){
     }    
 }
 /*
-void ImprimeDescricaoCasa(tCasa *casas, int i){
-    printf("\n\tDetalhes da casa %d\n", i);
-    printf("\t%d\n", casas.numPavimentos);
-    printf("\t%d\n", casas.numQuartos);
-    printf("\t%lf\n", casas.areaTerreno);
-    printf("\t%lf\n", casas.areaConstruida);
+void ImprimeDescricaoCasa(tCasa casas, FILE *imoveis){
+        while(1){
+                fread(&casas, 1, sizeof(tCasa),imoveis);
+                if(feof(imoveis)) break;
+                printf("\n\tDetalhes da casa %d\n");
+                printf("\t%d\n", casas.numPavimentos);
+                printf("\t%d\n", casas.numQuartos);
+                printf("\t%lf\n", casas.areaTerreno);
+                printf("\t%lf\n", casas.areaConstruida);
+       }
+}
+void ImprimeApartamento(tApartamento apartamentos, FILE *imoveis){
+
+    while(1){
+        fread(&apartamentos, 1, sizeof(tApartamento), imoveis);
+
+        if(feof(imoveis)) break;
+
+        printf("Apartamento:\n");
+        printf("\tEndereco: %s\n", apartamentos.dado.endereco);
+        printf("\tNumero: %d\n", apartamentos.dado.numero);
+        printf("\tBairro %s\n", apartamentos.dado.bairro);
+        printf("\tCep: %s\n", apartamentos.dado.cep);
+        printf("\tCidade: %s\n", apartamentos.dado.cidade);
+        printf("\tValor: %lf\n", apartamentos.dado.valor);
+        if(apartamentos.dado.disponivelAluVen == 'A'){
+            printf("\tapartamento disponivel para aluguel\n");
+        }else{
+            printf("Apartamento disponivel para venda\n");
+        }
+    }    
+}
+void ImprimeDescricaoApartamento(tApartamento apartamentos, FILE *imoveis){
+        while(1){
+                fread(&apartamentos, 1, sizeof(tApartamento),imoveis);
+                if(feof(imoveis)) break;
+                printf("\n\tDetalhes do apartamento %d\n");
+                printf("\t%lf\n", apartamentos.area);
+                printf("\t%d\n", apartamentos.numQuartos);
+                printf("\t%s\n", apartamentos.posicao);
+                printf("\t%lf\n", apartamentos.condominio);
+                printf("\t%d\n", apartamentos.vagasGaragem);
+       }
+}
+void ImprimeTerreno(tTerreno terrenos, FILE *imoveis){
+
+    while(1){
+        fread(&terrenos, 1, sizeof(tTerreno), imoveis);
+
+        if(feof(imoveis)) break;
+
+        printf("Terreno:\n");
+        printf("\tEndereco: %s\n", terrenos.dadoo.endereco);
+        printf("\tNumero: %d\n", terrenos.dadoo.numero);
+        printf("\tBairro %s\n", terrenos.dadoo.bairro);
+        printf("\tCep: %s\n", terrenos.dadoo.cep);
+        printf("\tCidade: %s\n", terrenos.dadoo.cidade);
+        printf("\tValor: %lf\n", terrenos.dadoo.valor);
+        if(terrenos.dadoo.disponivelAluVen == 'A'){
+            printf("\tterreno disponivel para aluguel\n");
+        }else{
+            printf("terreno disponivel para venda\n");
+        }
+    }    
+}
+void ImprimeDescricaoTerreno(tTerreno terrenos, FILE *imoveis){
+        while(1){
+                fread(&terrenos, 1, sizeof(tTerreno),imoveis);
+                if(feof(imoveis)) break;
+                printf("\n\tDetalhes do terreno\n");
+                printf("\t%lf\n", terrenos.area);
+       }
+}
+void ImoveisDisponiveis(tCasa *casas, tApartamento *apartamentos, tTerreno *terrenos, FILE *imoveis){
+        while(1){
+                fread(&casas, 1, sizeof(tCasa),imoveis);
+                if(feof(imoveis)) break;
+                if(casas.dados.disponivelAluVen != 'N' || casas.dados.disponivelAluVen != 'n'){
+                        printf("Casa disponivel.\n");
+                }
+        }
+        rewind(imoveis);
+        while(1){
+                fread(&apartamentos, 1, sizeof(tApartamento),imoveis);
+                if(feof(imoveis)) break;
+                if(apartamentos.dado.disponivelAluVen != 'N' || apartamentos.dado.disponivelAluVen != 'n'){
+                        printf("Apartamento disponivel.\n");
+                }
+        }
+        rewind(imoveis);
+        while(1){
+                fread(&terrenos, 1, sizeof(tTerreno),imoveis);
+                if(feof(imoveis)) break;
+                if(terrenos.dadoo.disponivelAluVen != 'N' || terrenos.dadoo.disponivelAluVen != 'n'){
+                        printf("Terreno disponivel.\n");
+                }
+        }
 }
 
-
-void ImoveisDisponiveis(tCasa *casas, tApartamento *apartamentos, tTerreno *terrenos){
-    int i;
-
-    for(i=0; i<100; i++){
-        if(casas[i].dados.disponivelAluVen != 'N' ||casas[i].dados.disponivelAluVen != 'n'){
-            printf("Casa %d disponivel.\n", i);
+void CasasDisponiveisVenda(tCasa *casas, FILE *imoveis){
+        rewind(imoveis);
+        while(1){
+                fread(&casas, 1, sizeof(tCasa),imoveis);
+                if(feof(imoveis)) break;
+                if(casas.dados.disponivelAluVen != 'V' || casas.dados.disponivelAluVen != 'v'){
+                        ImprimeCasa(casas, imoveis);
+                        ImprimeDescricaoCasa(casas, imoveis);
+                }
         }
-    }
+}
+
+void ApartamentosDisponiveisVenda(tApartamento *apartamentos, FILE *imoveis){
+        rewind(imoveis);
+        while(1){
+                fread(&apartamentos, 1, sizeof(tApartamento),imoveis);
+                if(feof(imoveis)) break;
+                if(apartamentos.dado.disponivelAluVen != 'V' || apartamentos.dado.disponivelAluVen != 'v'){
+                        ImprimeApartamento(apartamentos, imoveis);
+                        ImprimeDescricaoApartamento(apartamentos, imoveis);
+                }
+        }
+} 
+
+void TerrenosDisponiveisVenda(tTerreno *terrenos, FILE *imoveis){
+        rewind(imoveis);
+        while(1){
+                fread(&terrenos, 1, sizeof(tTerreno),imoveis);
+                if(feof(imoveis)) break;
+                if(terrenos.dadoo.disponivelAluVen != 'V' || terrenos.dadoo.disponivelAluVen != 'v'){
+                        ImprimeTerreno(terrenos, imoveis);
+                        ImprimeDescricaoTerreno(terrenos,imoveis);
+                }
+        }
+}
+
 
 }*/
 
