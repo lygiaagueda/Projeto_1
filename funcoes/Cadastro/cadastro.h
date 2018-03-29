7e5ee7ce53f8bb0eb6b40a92a7cddaa323a2c056
lygiaagueda <37081068+lygiaagueda@users.noreply.github.com>
2018-03-29 14:01:42 -0300

    Add files via upload

--- funcoes/Cadastro/cadastro.h
+++ funcoes/Cadastro/cadastro.h
@@ -90,7 +90,8 @@ void CadastraApartamento(tApartamento apartamentos, FILE *imoveis){
     printf("Digite o valor do apartamento: ");
     scanf("%lf", &apartamentos.dado.valor);
 
-    printf("Digite se o apartamento esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel): ");
+    printf("Digite se o apartamento esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel\n"
+            "N - nao disponivel para aluguel ou venda):");
     scanf("%c%*c", &apartamentos.dado.disponivelAluVen);
 
     printf("Digite a area do apartamento: ");
@@ -103,6 +104,9 @@ void CadastraApartamento(tApartamento apartamentos, FILE *imoveis){
     getchar();
     fgets(apartamentos.posicao, 100, stdin);
     apartamentos.posicao[strlen(apartamentos. posicao) - 1] = '\0';
+
+    printf("Digite o andar em que o apartamento se localiza: ");
+    scanf("%d", &apartamentos.numAndar);
 
     printf("Digite o valor do condominio do apartamentos: ");
     scanf("%lf", &apartamentos.condominio);
@@ -151,7 +155,8 @@ void CadastraTerreno(tTerreno terrenos, FILE *imoveis){
     printf("Digite o valor do terreno: ");
     scanf("%lf", &terrenos.dadoo.valor);
 
-    printf("Digite se o terreno esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel): ");
+    printf("Digite se o terreno esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel\n"
+            "N - nao disponivel para aluguel ou venda):");
     scanf("%c%*c", &terrenos.dadoo.disponivelAluVen);
 
     printf("Digite a area do terreno: ");
@@ -165,6 +170,148 @@ void CadastraTerreno(tTerreno terrenos, FILE *imoveis){
     } else {
         puts("Erro ao cadastrar terreno!");
     }
+}
+
+void CadastraFlat(tFlat flats, FILE *imoveis){
+    int verificador;
+
+    //Cadastrando flat
+    printf("\nCadastro do flat\n");
+    printf("Digite o nome da rua do flat: ");
+    getchar();
+    fgets(flats.dado.endereco, 100, stdin);
+    flats.dado.endereco[strlen(flats.dado.endereco) - 1] = '\0';
+
+    printf("Digite o numero do flat: ");
+    scanf("%d", &flats.dado.numero);
+
+    printf("Digite o nome do bairro do flat: ");
+    getchar();
+    fgets(flats.dado.bairro, 100, stdin);
+    flats.dado.bairro[strlen(flats.dado.bairro) - 1] = '\0';
+
+    printf("Digite o cep da rua do flat no formato 00000-000: ");
+    //getchar();
+    fgets(flats.dado.cep, 9, stdin);
+    flats.dado.cep[strlen(flats.dado.cep) - 1] = '\0';
+
+    printf("Digite o nome da cidade do flat: ");
+    //getchar();
+    fgets(flats.dado.cidade, 100, stdin);
+    flats.dado.cidade[strlen(flats.dado.cidade) - 1] = '\0';
+
+    printf("Digite o valor do flat: ");
+    scanf("%lf", &flats.dado.valor);
+
+    printf("Digite se o flat esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel)\n"
+            "N - para nao disponivel: ");
+    scanf("%c%*c", &flats.dado.disponivelAluVen);
+
+    printf("Digite a area do flat: ");
+    scanf("%lf", &flats.areaFlat);
+
+    printf("Digite oo valor do condominio do flat:");
+    scanf("%lf", &flats.valorCondominio);
+
+    printf("Digite se o flat possui ar condicionado (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.arCondicionado);
+
+    printf("Digite se o flat possui internet (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.internet);
+
+    printf("Digite se o flat possui TV a cabo (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.tvACabo);
+
+    printf("Digite se o flat possui servico de lavanderia (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.lavanderia);
+
+    printf("Digite se o flat possui servico de arrumacao/limpeza (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.limpeza);
+
+    printf("Digite se o flat possui servico de recepcao 24 horas (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &flats.recepcao24);
+
+    if(verificador == sizeof(flats)){
+        puts("Flat cadastrada com sucesso!");
+    } else {
+        puts("Erro ao cadastrar flat!");
+    }
+}
+
+void CadastraStudio(tStudio studios, FILE *imoveis){
+    int verificador;
+
+    //Cadastrando studio
+    printf("\nCadastro do studio\n");
+    printf("Digite o nome da rua do  studio: ");
+    getchar();
+    fgets(studios.dado.endereco, 100, stdin);
+    studios.dado.endereco[strlen(studios.dado.endereco) - 1] = '\0';
+
+    printf("Digite o numero do studio: ");
+    scanf("%d", &studios.dado.numero);
+
+    printf("Digite o nome do bairro do studio: ");
+    getchar();
+    fgets(studios.dado.bairro, 100, stdin);
+    studios.dado.bairro[strlen(studios.dado.bairro) - 1] = '\0';
+
+    printf("Digite o cep da rua do studio no formato 00000-000: ");
+    //getchar();
+    fgets(studios.dado.cep, 9, stdin);
+    studios.dado.cep[strlen(studios.dado.cep) - 1] = '\0';
+
+    printf("Digite o nome da cidade do studio: ");
+    //getchar();
+    fgets(studios.dado.cidade, 100, stdin);
+    studios.dado.cidade[strlen(studios.dado.cidade) - 1] = '\0';
+
+    printf("Digite o valor do studio: ");
+    scanf("%lf", &studios.dado.valor);
+
+    printf("Digite se o studio esta disponivel para venda ou aluguel (V - para venda/ A - para aluguel)\n"
+            "N - para nao disponivel: ");
+    scanf("%c%*c", &studios.dado.disponivelAluVen);
+
+    printf("Digite a area do studio:");
+    scanf("%lf", &studios.areaFlat);
+
+    printf("Digite oo valor do condominio do studio:");
+    scanf("%lf", &studios.valorCondominio);
+
+    printf("Digite se o studio possui ar condicionado (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.arCondicionado);
+
+    printf("Digite se o studio possui internet (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.internet);
+
+    printf("Digite se o studio possui TV a cabo (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.tvACabo);
+
+    printf("Digite se o studio possui servico de lavanderia (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.lavanderia);
+
+    printf("Digite se o studio possui servico de arrumacao/limpeza (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.limpeza);
+
+    printf("Digite se o studio possui servico de recepcao 24 horas (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.recepcao24);
+
+    printf("Digite se o studio possui piscina (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.piscina);
+
+    printf("Digite se o studio possui sauna (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.sauna);
+
+    printf("Digite se o studio possui sala de ginastica (S - para possui/ N - para nao possui):");
+    scanf("%c%*c", &studios.salaGinastica);
+
+    if(verificador == sizeof(studios)){
+        puts("Studio cadastrada com sucesso!");
+    } else {
+        puts("Erro ao cadastrar studio!");
+    }
+
 }
 
 #endif
