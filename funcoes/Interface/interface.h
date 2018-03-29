2dab798f672e112dd4c556ab1d11dbebd002beea
lygiaagueda <37081068+lygiaagueda@users.noreply.github.com>
2018-03-29 14:02:26 -0300

    Add files via upload

--- funcoes/Interface/interface.h
+++ funcoes/Interface/interface.h
@@ -23,7 +23,45 @@ void ExibeMenuSubItem1(){
     printf("1 - Cadastrar casa\n");
     printf("2 - Cadastrar apartamento\n");
     printf("3 - Cadastrar terreno\n");
-    printf("4 - Voltar\n");
+    printf("4 - Cadastrar flat\n");
+    printf("5 - Cadastrar studio\n\n");
+    printf("6 - Voltar\n");
+    printf("-------------------------------------------------\n");
+}
+
+void ExibeMenuSubItem4(){
+    printf("-------------------------------------------------\n");
+    printf("Menu de imoveis disponveis para venda\n");
+    printf("-------------------------------------------------\n");
+    printf("1 - Casas disponveis\n");
+    printf("2 - Apartamentos disponiveis\n");
+    printf("3 - Terrenos disponveis\n");
+    printf("4 - Flats disponiveis\n");
+    printf("5 - Studio dispoiveis\n\n");
+    printf("6 - Voltar\n");
+    printf("-------------------------------------------------\n");
+}
+
+void ExibeMenuSubItem5(){
+    printf("-------------------------------------------------\n");
+    printf("Menu de imoveis disponveis para alugar\n");
+    printf("-------------------------------------------------\n");
+    printf("1 - Casas disponveis\n");
+    printf("2 - Apartamentos disponiveis\n");
+    printf("3 - Terrenos disponveis\n");
+    printf("4 - Flats disponiveis\n");
+    printf("5 - Studio dispoiveis\n\n");
+    printf("6 - Voltar\n");
+    printf("-------------------------------------------------\n");
+}
+
+void ExibeMenuSubSubItem5(){
+    printf("-------------------------------------------------\n");
+    printf("Menu de imoveis disponveis para alugar\n");
+    printf("-------------------------------------------------\n");
+    printf("1 - Todos os imoveis\n");
+    printf("2 - Pesquisar por bairro\n\n");
+    printf("3 - Voltar\n");
     printf("-------------------------------------------------\n");
 }
 
